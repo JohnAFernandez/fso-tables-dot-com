892527e89187337b3b4f8b146a991cd23ab82628
John Fernandez <jafernandez.tampabay@gmail.com>
2024-11-29 18:30:46 -0500

    Handle LoginModal Opening correctly

--- main/js/login-scritps.js
+++ main/js/login-scritps.js
@@ -1,30 +1,31 @@
 function onLoginModalOpen() {
-    const passwordField = document.getElementById("loginPassword");
-    const checkbox = document.getElementById("loginPasswordToggleShowPassword");
+  const loginForm = document.getElementById("login-form");
+  const passwordField = document.getElementById("loginPassword")
+  const checkbox = document.getElementById("loginPasswordToggleShowPassword");
 
-    passwordField.type = "reset";
-    passwordField.type = "password";
-    checkbox.checked = false;
-
-    passwordField.nodeValue = "";
+  // When the modal is reloaded, make sure to erase the password so that it's not 
+  // some rando gaining access to the accidentally abandoned password
+  passwordField.value = "";
+  passwordField.type = "password";
+  checkbox.checked = false;
 }
 
 $(window).on('shown.bs.modal', onLoginModalOpen);
 
 function togglePasswordLogin() {
-    const passwordField = document.getElementById("loginPassword");
-    const checkbox = document.getElementById("loginPasswordToggleShowPassword");
+  const passwordField = document.getElementById("loginPassword");
+  const checkbox = document.getElementById("loginPasswordToggleShowPassword");
 
-    if (passwordField.type === "password") {
-      console.log("Setting visible.");
+  if (passwordField.type === "password") {
+    console.log("Setting visible.");
 
-      passwordField.type = "text";
-      checkbox.checked = true;
-    } else {
-      console.log("Setting hidden.");
-      passwordField.type = "password";
-      checkbox.checked = false;
-    }
+    passwordField.type = "text";
+    checkbox.checked = true;
+  } else {
+    console.log("Setting hidden.");
+    passwordField.type = "password";
+    checkbox.checked = false;
+  }
 }
 
 function attemptLogin(email, password) {
@@ -32,8 +33,3 @@ function attemptLogin(email, password) {
 }
 
 
-
-function validateEmail(email) {
-    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-}
