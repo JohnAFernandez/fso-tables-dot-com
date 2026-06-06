169e29fae0fc5af6322aedeffa7e8979251e4d04
John Fernandez <jafernandez.tampabay@gmail.com>
2026-06-06 14:59:55 -0400

    Implement item Submission Part 2

--- main/js/edit-scrpits.js
+++ main/js/edit-scrpits.js
@@ -1,3 +1,5 @@
+let awaitingSubmissionResult = false;
+
 function initiateItemEdit(id) {
     /*
     item${id}
@@ -53,3 +55,62 @@ function saveItemEditChanges(id){
     console.log(`Call to editing function done. id ${id}`)
 }
 
+function clearItemSubmitResultText(waiting){
+    awaitingSubmissionResult = waiting;
+}
+
+function submit_new_item(){
+  if (awaitingSubmissionResult === true) {
+    return;
+  }
+
+  clearItemSubmitResultText();
+  awaitingSubmissionRequest(true);
+
+  const textField = document.getElementById("new-item-name");
+  const docField = document.getElementById("new-item-documentation");
+  const majorVersionField = document.getElementById("new-item-major");
+  const parentIdField = document.getElementById("parent-item");
+  const tableIdField = document.getElementById("loginEmail");
+  const infoTypeField = document.getElementById("new-item-type");
+  const defaultValueField = document.getElementById("loginEmail");
+  const tableIndexField = document.getElementById("loginEmail");
+
+
+  const addItemRequest = {
+    email: emailField.value,
+    password: passwordField.value,
+  }
+
+  fetch(API_ROOTB + "users/login", {
+    method: "POST",
+    body: JSON.stringify(loginRequest),
+    credentials: "include"
+  })
+  .then((response) => { 
+    if (response.status === 200) {
+      // Default login expiration of a week.
+      setCookie("username", loginRequest.email, 7*24)
+
+      check_login_status_and_update();
+      dismissLoginModal();
+    } else {
+      response.json().then(responseJSON => { 
+        // if we didn't have a success then, there was an error from the server, and we should be displaying what it sent. 
+        throw responseJSON.Error;}
+      ).catch(
+        error => { 
+          console.log(`Login failed. The error encountered was: ${error}`);
+          awaitingSubmissionRequest(false);
+          setLoginErrorText(`${error}`);
+      })
+    }
+  }).catch ( 
+    error => { 
+      console.log(`Login in failed due to some server or network error. The error encountered was: ${error}`);
+      awaitingSubmissionRequest(false);
+      setLoginErrorText("Login Failed, Server or Network Error");
+    }
+  );
+   
+}
