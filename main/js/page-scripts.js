16f2db136807e9fad9df98ae7e2537fe8f0fdf5e
John Fernandez <jafernandez.tampabay@gmail.com>
2026-06-11 11:17:43 -0400

    I guess this doesn't remove old text afterall.

--- main/js/page-scripts.js
+++ main/js/page-scripts.js
@@ -719,11 +719,11 @@ async function apply_table(table) {
         child.setAttribute("id", `item${i}-documentation`);
 
         if (deprecation === false && alias === false){
-          child.textContent = ` No aliases and not deprecated.`;
+          child.textContent = `No aliases and not deprecated.`;
         } else if (deprecation === false){
-          child.textContent = ` Not deprecated.`;
+          child.textContent = `Not deprecated.`;
         } else if (alias === false) {
-          child.textContent = ` No Aliases`;
+          child.textContent = `No Aliases`;
         } else {
           child.style.display = "none";
         }
