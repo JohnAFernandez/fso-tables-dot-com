78232f660d2298d9c38128942f69337b411fe861
John Fernandez <jafernandez.tampabay@gmail.com>
2026-07-26 23:30:51 -0400

    Change order of enabling and disabling for copying

--- main/js/edit-scrpits.js
+++ main/js/edit-scrpits.js
@@ -38,11 +38,11 @@ function initiateItemEdit(id) {
   toggleContents(true, `item${id}-edit-major-version-group`);
 
   // Description
+  toggleContents(true, `item${id}-edit-description-area`);
   current_element = document.getElementById(`item${id}-documentation`);
   target_element = document.getElementById(`item${id}-edit-description`);
   changeContents(`item${id}-edit-description`, current_element.innerText);
   toggleContents(false, `item${id}-documentation`);
-  toggleContents(true, `item${id}-edit-description-area`);
 
   // type
   current_element = document.getElementById(`item${id}-variable-type`);
